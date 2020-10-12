2a7d7008c9823d34329e41b97d82e3720dc6eceb
isogreen42 <45437825+isogreen42@users.noreply.github.com>
2020-10-12 10:50:54 -0400

    Update WOPINS Network Transformation Script.R

--- WOPINS Network Transformation Script.R
+++ WOPINS Network Transformation Script.R
@@ -1,10 +1,11 @@
 #################################################################################
 #
-# This R script is used to transform the adjacency matrix data from the 
-# three units of women into 'wide' form data that can be easily merged 
-# with the survey datafile. This will allow users to calculate values for a 
-# survey respondents 'network alters' with out needing familiarity with
-# social network methods.
+# This R script is used to transform simple adjacency matrix data into a wide
+# variable and write it to a .CSV file for merging with other statistical software 
+# This will allow users to calculate values for a survey respondents 
+# 'network alters' with out needing familiarity with
+# social network methods. This script was devoloped for the WOPINS data 
+# submission to ICPSR.
 #
 # This script was written in R studio. Users should download R and R Studio 
 # before proceeding.
@@ -14,32 +15,27 @@
 # 
 # The following steps are coded below;
 # 1: Installing and loading the required R packages for the transformation.
-# 2: R code to generate a global function to transform the network.
-# 3: Specifying parameters unique to the end user.
+# 2: Specifying parameters unique to the end user.
+# 3: R code to generate a global function to transform the network.
 # 4: Running the function and listing the files in the working directory. 
 
-# Section 3 is the only place where user changes need occur.  
+#===============================================================================
+# Section 2 is the only place where user changes need occur.  
+#===============================================================================
 
 #################################################################################
 
 # The code included here will *NOT* generate an alter variable for all of the 
-# members of the unit.
-# The output file will only include unit members who selected at least one 
-# 'network alter'
-# for any given social network measured. Otherwise, they are not included in the 
-# 'edgelist' transformation
+# members of the network.
+# The output file will only include network members who selected at least one 
+# 'network alter' for any given social network measured. 
+# Otherwise, they are not included in the 'edgelist' transformation
 # within the function below. These 'non-network respondents' can be identified 
-# when merging the output 
-# file back to the survey data. Likewise, the variables in the output data are 
-# only for survey respondents.
+# when merging the output file back to the survey data. 
+# Likewise, the variables in the output data are only for survey respondents.
 
-# Additionally, this script is intended only for the following networks;
-# "Get Along With", "Prison Family", "Power and Influence", 
-# and "Support During a Dispute".
-
-# The "Prison Family Role" and "Power and Influence Affect" are specialized 
-# matrices where the cell value contains information about the network. 
-# This script does not handle these networks. 
+# Additionally, this script is intended only for simple adjacency matrices where 
+# the cell values are 0 or 1. 
 
 ################################################################################# 
 #0.0 - Clear the Workspace
@@ -61,7 +57,26 @@ library(dplyr)
 library(igraph)
 
 ################################################################################# 
-#2.0 - This block of code creates a global function to use in R called Transform
+# 2.0 - End User Parameters                                                     #
+#                                                                               #
+#=================Specify 3 Parameters Specific to the End User ================#
+                                                                                #
+# 1 - User's working directory - use front slashes, not back slashes            #
+                                                                                #
+myFilePath <- setwd("C:/")
+                                                                                #
+# 2 - The network to be transformed - change for each new network               #
+                                                                                #
+myNetwork <- "myFileName.csv" 
+                                                                                #
+# 3 - The output file name - change for each new network                        #
+                                                                                #
+myOutputName <- "myOutputFile.csv"
+                                                                                #
+#===============================================================================#
+
+################################################################################# 
+# 3.0 - This block of code creates a global function to use in R called Transform
 
 Transform <- function(df, type = matrix){
   setwd(myFilePath)                                                                 
@@ -90,34 +105,18 @@ Transform <- function(df, type = matrix){
   rm(list = objs[grep("^my",objs)], pos = ".GlobalEnv")
 }
 
-################################################################################# 
-# 3.0 - End User Parameters                                                     #
-#                                                                               #      
-#=================Specify 3 Items Specific to the End User =====================#
-                                                                                #
-# 1 - User's working directory - use front slashes, not back slashes            #
-                                                                                #
-myFilePath <- setwd("C:/Users/theod/Desktop/testEnviroment")
-                                                                                #
-# 2 - The network to be transformed - change for each new network               #
-                                                                                #
-myNetwork <- "Unit3_SDD.csv" 
-                                                                                #
-# 3 - The output file name - change for each new network                        #
-                                                                                #
-myOutputName <- "SupportNet.csv"
-                                                                                #
-#===============================================================================#
-
-# Running the Function created above, writes a .csv file to the working 
-# directory, and creates a data frame in R called 'wideData' for the user to view.
-
+# Running the function created above, writes a .csv file to the working 
+# directory, and creates a data frame in R called 'wideData' for the user 
+# to view.
 # The function is designed to remove the user specified objects to prevent over-
-# writing the transformed files. Change the three user parameters, and re-run this
-# whole R script. Recommend ctrl-A, ctrl-ENTER
+# writing the transformed files. Change the three user parameters, and re-run 
+# this whole R script. Recommend ctrl-A, ctrl-ENTER. 
+# To run lines of the function individually to follow the process, 
+# delete lines 85, 107-110. 
 
-# 4.0
+################################################################################# 
+# 4.0 - Transforms the network read in by the function
 Transform(network)
 
-# 4.1
+# 4.1 - Prints the files in the working directory 
 list.files(getwd())
